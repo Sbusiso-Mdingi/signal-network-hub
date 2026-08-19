503c29f6f46bb4be98ecfd60693a46aab89745bb
Sbusiso Mdingi <ss.mdingi@icloud.com>
2026-08-20 01:11:27 +0200

    chore: migrate public links to sequrin.com

--- src/lib/siteLinks.ts
+++ src/lib/siteLinks.ts
@@ -1,3 +1,3 @@
-export const APP_SIGN_IN_URL = "https://app.sequrin.tech/sign-in";
-export const PUBLIC_CONTACT_EMAIL = "info@sequrin.tech";
+export const APP_SIGN_IN_URL = "https://app.sequrin.com/sign-in";
+export const PUBLIC_CONTACT_EMAIL = "info@sequrin.com";
 export const PUBLIC_CONTACT_MAILTO = `mailto:${PUBLIC_CONTACT_EMAIL}`;
